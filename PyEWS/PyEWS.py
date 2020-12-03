bea7a64a4e08ed61af5c7133967088ee8780aca0
Marco Polo Jácome Toss <jacometoss@gmail.com>
2020-12-02 22:51:27 -0600

    Python Electrical Wire Sizes 1.14

--- PyEWS/PyEWS.py
+++ PyEWS/PyEWS.py
@@ -2,15 +2,13 @@ from tabulate import tabulate
 import math
 
 '''
-
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-| PYEWS, ElectricalWireSizes, 09/11/2020                                 |
-| Version : 0.1.12                                                       |
+| PYEWS, Electrical Wire Sizes, 20/11/2020                               |
+| Version : 0.1.13                                                       |
 | Autor : Marco Polo Jacome Toss                                         |
 | License: GNU Affero General Public License v3 (GPL-3.0)                |
 | Requires: Python >=3.5                                                 |
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
 '''
 dbConductor=[["14 AWG",10.171,0.1903,10.171,0.1903,10.171,0.2395,20,20,25],
 ["12 AWG",6.5617,0.1772,6.5617,0.1772,6.5617,0.2231,25,25,30],
@@ -47,7 +45,7 @@ def Z(R,X,FP):
 def DBC():
     print(tabulate(dbConductor, headers=["AWG/KCM","R(Ω/km)", "X (Ω/km)","R (Ω/km)","X (Ω/km)", "R (Ω/km)", "X (Ω/km)", "60°C", "75°C", "90°C"], tablefmt='psql'))
     
-def MBTCU(VF,VL,In,Nc,L,FA,Type,Ta,Vd,S,Fp):
+def MBTCU(VF,VL,In,Nc,L,FA,Type,Ta,Vd,S,Fp,View):
 
     if Ta >= 60:
         FT60=0.0
@@ -108,8 +106,7 @@ def MBTCU(VF,VL,In,Nc,L,FA,Type,Ta,Vd,S,Fp):
     ["750 KCM"],
     ["1000 KCM"]]
 
-
-
+    
 
     #for i in range(len(dbConductor)):
     #datos[i].append(round(Rn(dbConductor[i][1],75),4))
@@ -169,9 +166,9 @@ def MBTCU(VF,VL,In,Nc,L,FA,Type,Ta,Vd,S,Fp):
             if Vd > D1:
                 
                 if ((round(datos[i][5],3)*FA*FT60>In) or (round(datos[i][5],3)*FA*FT75>In) or (round(datos[i][5],3)*FA*FT90>In)):
-                    datos[i].append(' Yes')
+                    datos[i].append('Yes')
                 else:
-                    datos[i].append(' Not')
+                    datos[i].append('Not')
                     
             else:
                 datos[i].append('Not')
@@ -208,9 +205,9 @@ def MBTCU(VF,VL,In,Nc,L,FA,Type,Ta,Vd,S,Fp):
                 
                 if ((round(datos[i][5],3)*FA*FT60>In) or (round(datos[i][5],3)*FA*FT75)>In or (round(datos[i][5],3)*FA*FT90>In)):
                     
-                    datos[i].append(' Yes')
+                    datos[i].append('Yes')
                 else:
-                    datos[i].append(' Not')
+                    datos[i].append('Not')
             else:
                 datos[i].append('Not')
             
@@ -247,9 +244,9 @@ def MBTCU(VF,VL,In,Nc,L,FA,Type,Ta,Vd,S,Fp):
                 
                 if ((round(datos[i][5],3)*FA*FT60>In) or (round(datos[i][5],3)*FA*FT75>In) or (round(datos[i][5],3)*FA*FT90>In)):
                     
-                    datos[i].append(' Yes')
+                    datos[i].append('Yes')
                 else:
-                    datos[i].append(' Not')
+                    datos[i].append('Not')
                     
             else:
                 datos[i].append('Not')
@@ -295,6 +292,129 @@ def MBTCU(VF,VL,In,Nc,L,FA,Type,Ta,Vd,S,Fp):
                 if (SITM[j]>Nc*In*1.25):
                     datos[i].append(SITM[j])
                     break
-        
-    print(tabulate(datos, headers=["AWG/KCM","1F/2H", "2F/3H","3F/3H","3F/4H", "60", "75", "90","%Vd/1F", "%Vd/2F","%Vd/3F","%Vd/3F","Nc", "In", "60", "75", "90", "Op", "ITM"], tablefmt='psql'))
+    if View == 1:
+        #Mostrar información en PSQL
+        print(tabulate(datos, headers=["AWG/KCM","1F/2H", "2F/3H","3F/3H","3F/4H", "60", "75", "90","%Vd/1F", "%Vd/2F","%Vd/3F","%Vd/3F","Nc", "In", "60", "75", "90", "Op", "ITM"], tablefmt='psql'))
+    elif View == 2:
+        #Mostrar la información en lista
+        return datos
+    
+    
+    
+def DBCIRCUIT(carga,view):
 
+    if len(carga)==1:
+        dbcircuit=[["1"]] 
+    elif len(carga)==2:
+        dbcircuit=[["1"],["2"]] 
+    elif len(carga)==3:
+        dbcircuit=[["1"],["2"],["3"]] 
+    elif len(carga)==4:
+        dbcircuit=[["1"],["2"],["3"],["4"]] 
+    elif len(carga)==5:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"]] 
+    elif len(carga)==6: 
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"]]     
+    elif len(carga)==7:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"]] 
+    elif len(carga)==8:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"]]     
+    elif len(carga)==9:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"]]     
+    elif len(carga)==10:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"]] 
+    elif len(carga)==11:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"]] 
+    elif len(carga)==12:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"]] 
+    elif len(carga)==13:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"]] 
+    elif len(carga)==14:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"]] 
+    elif len(carga)==15:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"]]    
+    elif len(carga)==16:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"]]     
+    elif len(carga)==17:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"],["17"]] 
+    elif len(carga)==18:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"],["17"],["18"]] 
+    elif len(carga)==19:
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"],["17"],["18"],["19"]]     
+    elif len(carga)==20: 
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"],["17"],["18"],["19"],["20"]]     
+    elif len(carga)==21: 
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"],["17"],["18"],["19"],["20"],["21"]]          
+    elif len(carga)==22: 
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"],["17"],["18"],["19"],["20"],["21"],["22"]]          
+    elif len(carga)==23: 
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"],["17"],["18"],["19"],["20"],["21"],["22"],["23"]]         
+    elif len(carga)==24: 
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"],["17"],["18"],["19"],["20"],["21"],["22"],["23"],["24"]]
+    elif len(carga)==25: 
+        dbcircuit=[["1"],["2"],["3"],["4"],["5"],["6"],["7"],["8"],["9"],["10"],["11"],["12"],["13"],["14"],["15"],["16"],["17"],["18"],["19"],["20"],["21"],["22"],["23"],["24"],["25"]]
+                
+        
+        
+    datos=[]  
+    for i in range(len(carga)):
+        datos.append(MBTCU(carga[i][1],carga[i][2],carga[i][3],carga[i][4],carga[i][5],carga[i][6],carga[i][7],carga[i][8],carga[i][9],carga[i][10],carga[i][11],carga[i][12])) 
+        if view==1:
+            print("Id [",i+1,"]========================================================================================================================================================================")
+            print(tabulate(datos[i], headers=["AWG/KCM","1F/2H", "2F/3H","3F/3H","3F/4H", "60", "75", "90","%Vd/1F", "%Vd/2F","%Vd/3F","%Vd/3F","Nc", "In", "60", "75", "90", "Op", "ITM"], tablefmt='psql'))
+        #elif view==2:
+        #    print("Id [",i+1,"] View = 1 PyEWS.DBCUIRCUIT(carga,1)") 
+            
+                
+    for i in range(len(carga)):
+        for j in range(len(dbConductor)):
+        
+            if datos[i][j][17]=="Yes":
+                dbcircuit[i].append(datos[i][j][0])
+                #dbcircuit[i].append(datos[i][j][1])
+                dbcircuit[i].append(carga[i][5])
+                dbcircuit[i].append(carga[i][9])
+                dbcircuit[i].append(carga[i][11])
+             
+                
+                if carga[i][10]==1:
+                    dbcircuit[i].append("1F/2H")
+                elif carga[i][10]==2:
+                    dbcircuit[i].append("2F/3H")
+                elif carga[i][10]==3:
+                    dbcircuit[i].append("3F/3H")
+                elif carga[i][10]==4:
+                    dbcircuit[i].append("3F/3H")
+                    
+                #dbcircuit[i].append(datos[i][j][2])
+                #dbcircuit[i].append(datos[i][j][3])
+                #dbcircuit[i].append(datos[i][j][4])
+                dbcircuit[i].append(datos[i][j][5])
+                dbcircuit[i].append(datos[i][j][6])
+                dbcircuit[i].append(datos[i][j][7])
+                
+                if carga[i][9]==1:
+                    dbcircuit[i].append(datos[i][j][8])
+                elif carga[i][9]==2:
+                    dbcircuit[i].append(datos[i][j][9])
+                elif carga[i][9]==3:
+                    dbcircuit[i].append(datos[i][j][10])
+                elif carga[i][9]==4:
+                    dbcircuit[i].append(datos[i][j][11])
+                
+                
+                dbcircuit[i].append(datos[i][j][12])
+                dbcircuit[i].append(datos[i][j][13])
+                dbcircuit[i].append(datos[i][j][14])
+                dbcircuit[i].append(datos[i][j][15])
+                dbcircuit[i].append(datos[i][j][16])
+                #dbcircuit[i].append(datos[i][j][17])
+                dbcircuit[i].append(datos[i][j][18])
+                break
+
+            
+
+    #return dbcircuit
+    print("::::::: [ RESUMEN DE CARGAS ]:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
+    #print(tabulate(dbcircuit, headers=["Id","AWG/KCM","l", "2F/3H","3F/3H","3F/4H", "60", "75", "90","%Vd/1F", "%Vd/2F","%Vd/3F","%Vd/3F","Nc", "In", "60", "75", "90", "Op", "ITM"], tablefmt='psql'))
+    print(tabulate(dbcircuit, headers=["Id","#CAL","L[m]", "Vd","FP","ALM", "60", "75", "90","Vd[%]","Nc", "In", "60", "75", "90", "ITM"], tablefmt='psql'))
--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-![PyEWS](https://i.ibb.co/YD4XKb8/02.png)
+<img src="https://i.ibb.co/YD4XKb8/02.png" alt="PyEWS" style="zoom: 80%;" />
 
 
 
 # PyEWS - Python Electrical Wire Sizes 
 
-
-
-El módulo PyEWS (0.1.12) sirve para obtener el dimensionamiento de conductores para una instalación eléctrica. Es fácil de utilizar e interpretar sus resultados generando un panorama más general al poder visualizar por completo una lista de conductores con los parámetros de entrada.
+El módulo PyEWS (0.1.14) sirve para obtener el dimensionamiento de conductores para una instalación eléctrica. Es fácil de utilizar e interpretar sus resultados generando un panorama más general al poder visualizar por completo una lista de conductores con los parámetros de entrada.
 
 ## Instalación
 
@@ -35,19 +33,23 @@ El módulo tiene dependencias siendo necesario instalar `tabulate` el cual da un
 #S:  Seleccione el sistema que desea propner en base a este se muestran los resultados.
 ---- 1:(1F-2H) 2:(2F-3H) 3:(3F-3H) 4:(3F-4H)
 #Fp: Factor de potencia
+#Op: Opción para mostrar resultados
+	 1: Mostrar los resultados adecuadamente estructurado en una tabla. 
+	 2: Mostrar los resultados como datos acumulados. Esta opción es necesario cuando se activa
+	 	la función para múltiples cargas.
 	
 ```
 
 ```python
 import PyEWS 
-PyEWS.MBTCU(VF,VL,In,Nc,L,FA,Type,Ta,Vd,S,Fp)
+PyEWS.MBTCU(VF,VL,In,Nc,L,FA,Type,Ta,Vd,S,Fp,Op)
 ```
 
 ## PyEWS Módulos
 
 | Id   | Descripción                                                  | Módulo | Versión |                      Descargar                      |
 | ---- | ------------------------------------------------------------ | ------ | ------- | :-------------------------------------------------: |
-| 1    | Módulo de baja tensión para conductores de cobre clase B,  tensión de 600V a 2000V | MBTCU  | 0.1.12  | [PyEWS 0.1.12](https://github.com/jacometoss/PyEWS) |
+| 1    | Módulo de baja tensión para conductores de cobre clase B,  tensión de 600V a 2000V | MBTCU  | 0.1.14  | [PyEWS 0.1.14](https://github.com/jacometoss/PyEWS) |
 
 ## Test
 
@@ -55,7 +57,7 @@ El módulo tiene dependencias por lo que es necesario instalar `tabulate` el cua
 
 ```python
 import PyEWS
-PyEWS.MBTCU(127,220,25,1,35,1,1,2,3,1,0.9)
+PyEWS.MBTCU(127,220,15,1,22,1,1,35,3,1,0.9,1)
 ```
 
 Los se resultados muestran con la iteración de todos los conductores tanto para tensión monofásica como trifásica.
@@ -66,7 +68,9 @@ Los se resultados muestran con la iteración de todos los conductores tanto para
 - `Op` muestra si el resultado es correcto al aparecerer en la columna como Yes.
 - `ITM` es la protección del circuito.
 
-![Resultados](https://i.ibb.co/KDwv9jX/01.png)
+![Resultados](https://i.ibb.co/VDtY0vz/Ejemplo-01.jpg)
+
+El circuito mostrado es la solución y para ver el resumen este fue agregado para la opción multiples cargas.
 
 ## Base de datos de conductores
 
@@ -77,7 +81,54 @@ import PyEWS
 PyEWS.DBC()
 ```
 
+## Múltiples cargas 
+
+Para implementar una gran variedad de cargas se organizan como se muestra en el bloque de código, puede agregar hasta 25 cargas.
+
+```python
+carga=[
+     ["1",127,220,15,1,22,1,1,35,3,1,0.9,2],
+     ["2",127,220,12,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["1",127,220,15,1,22,1,1,35,3,1,0.9,2],    
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["1",127,220,15,1,22,1,1,35,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],    
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+     ["2",127,220,22,1,22,1,1,25,3,1,0.9,2],
+    ["3",127,220,196,3,55,0.8,3,75,3,3,0.9,2]
+    ]
+#Una forma sencilla de mostrar el total de cargas
+print("Total de cargas : ",len(carga))
+#Para mostar completo el desarrollo
+#----------PyEWS.DBCIRCUIT(carga,1)
+#Para mostar el resumen únicamente 
+#----------PyEWS.DBCIRCUIT(carga,2)
+```
+
+Para mostrar el resumen únicamente
+
+```python
+PyEWS.DBCIRCUIT(carga,2)
+```
+
+![Resultados](https://i.ibb.co/hWV33Vj/Captura.jpg)
+
+
+
 ## Referencias
 
-1. [Norma Oficial Mexicana NOM-001-SEDE-2012, Instalaciones Eléctricas (utilización)](http://www.issste-cmn20n.gob.mx/Datos/Normas/136NOM.pdf)
+[1] Norma Oficial Mexicana NOM-001-SEDE-2012, Instalaciones Eléctricas (utilización)
+
+[2] Thue, W., 1978. *Electrical Power Cable Engineering*. 2nd ed. New York, Basel: Marcel Dekker Inc., p.34.
 

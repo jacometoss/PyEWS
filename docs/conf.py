26f2113be1e3f751fad7f1cfea52993bc268ab6b
Marco Polo Jácome Toss <jacometoss@gmail.com>
2021-05-31 00:04:08 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -219,7 +219,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#####latex_logo = '_static/covantec.jpg'
+latex_logo = '_static/logo.jpg'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.

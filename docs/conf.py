0ae8eca0797cc2068bdb8dd9369f945a703a75d8
Marco Polo Jácome Toss <jacometoss@gmail.com>
2021-05-30 23:20:29 -0500

    Config

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ today_fmt = '%d de %B de %Y'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['index_latex.rst']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None

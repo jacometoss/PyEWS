3c4c699a39b1b1dc93ffcc88c4c8e926ea676ab0
Marco Polo Jácome Toss <jacometoss@gmail.com>
2022-05-12 20:51:54 -0500

    0.1.27

--- docs/conf.py
+++ docs/conf.py
@@ -98,8 +98,8 @@ pygments_style ='friendly'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinxdoc'
 #html_theme = 'bizstyle'
 #html_theme = 'kotti_docs_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
